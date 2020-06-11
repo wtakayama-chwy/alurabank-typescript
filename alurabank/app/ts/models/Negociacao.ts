33bd536ad88230aa8a2bdec50460dde617e1a508
William S. Takayama <william-sobreira@Hotmail.com>
2020-06-11 00:24:58 -0300

    Setting up typscript basics

---
+++ alurabank/app/ts/models/Negociacao.ts
@@ -0,0 +1,26 @@
+class Negociacao {
+
+    private _data;
+    private _quantity;
+    private _value;
+
+    constructor(data, quantity, value) { 
+
+        this._data = data;
+        this._quantity = quantity;
+        this._value = value;
+    }
+
+    get data() {
+        return this._data;
+    }
+    get quantity() {
+        return this._quantity;
+    }
+    get value() {
+        return this._value;
+    }
+    get volume() {
+        return this._quantity * this._value;
+    }
+}
